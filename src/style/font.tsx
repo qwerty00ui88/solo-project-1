7f56b8f4b107dbe71308e330c4df62fa1edc4bc9
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2023-09-11 15:39:52 +0900

    deploy: workflow test

--- src/style/font.tsx
+++ src/style/font.tsx
@@ -1,10 +1,3 @@
-// export const xxsmallSize = '0.9rem'
-// export const xsmallSize = '1rem'
-// export const smallSize = '1.2rem'
-// export const mediumSize = '1.5rem'
-// export const largeSize = '1.7rem'
-// export const xlargeSize = '2.5rem'
-
 export const lightWeight = 300
 export const regularWeight = 400
 export const mediumWeight = 500
